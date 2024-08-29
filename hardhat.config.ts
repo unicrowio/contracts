d12e0ce08c1d043318d8869e29d7247e0d49ef52
Michele Federici (@ps1dr3x) <michele@federici.tech>
2024-08-29 14:42:11 +0200

    migrate types to ethers v6 (typechain)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -81,7 +81,7 @@ const userConfig: HardhatUserConfig = {
   },
   typechain: {
     outDir: "types",
-    target: "ethers-v5",
+    target: "ethers-v6",
     alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
     externalArtifacts: ["artifacts/contracts/**/.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
